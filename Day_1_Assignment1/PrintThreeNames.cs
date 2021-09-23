c070f04d0f1366dfe3795677651e0d85b361ca60
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-23 23:28:44 +0530

    [Ravina] Added : Method to print names in sorted order

---
+++ Day_1_Assignment1/PrintThreeNames.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_1_Assignment1
+{
+    class PrintThreeNames
+    {
+
+        public void printNames()
+        {
+
+
+            int n = 3;
+
+            string[] arr = new string[n];
+            Console.Write("Enter strings below  = ");
+            for (int i = 0; i<n; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            
+
+            for (int i = 0; i<arr.Length; i++)
+            {
+               
+
+                for (int j = 0; j<arr.Length - 1; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                       string temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                   
+                }
+            }
+            Console.Write("After sorting the array appears like : Hi ");
+           for ( int i = 0; i < arr.Length; i++)
+           {
+                Console.Write(arr[i] + "," + " ");
+
+           }
+         Console.WriteLine();
+        }
+    }
+}
