d211f4575b07082d7fa3902af6f77689b529336e
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-23 21:31:58 +0530

    [Ravina] Added : Method to check date and month

---
+++ Day_1_Assignment1/SpringSeason.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_1_Assignment1
+{
+    class SpringSeason
+    {
+
+        public void CheckDayAndMonth()
+        {
+            int date, month;
+
+            Console.WriteLine("Checking date and month \n");
+            
+
+            Console.WriteLine("Enter date = ");
+            date = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Month = ");
+            month = Convert.ToInt32(Console.ReadLine());
+
+            if ((date <=20 || month  <= 3 ) && (date >=20 || month <=6 ) && (date <=31))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
