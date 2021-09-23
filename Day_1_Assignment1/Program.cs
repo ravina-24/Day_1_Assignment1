a1eef0201b4b99c74581e0cdb6d4915de1fdcee0
ravina-24 <ravinamaudekar24@gmail.com>
2021-09-23 23:29:11 +0530

    [Ravina] Added : Main Program file

--- Day_1_Assignment1/Program.cs
+++ Day_1_Assignment1/Program.cs
@@ -7,19 +7,49 @@ namespace Day_1_Assignment1
     {
         static void Main(string[] args)
         {
+
+            int input;
+            Console.WriteLine("Enter input = \n " +
+                "1. PrintPattern \n "  +
+                "2.LeapYear \n" +
+                "3. Spring Season \n" +
+                "4.Quadratic equation \n" +
+                "5. printNames  ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+
+                case 1:
             PrintPattern p = new PrintPattern();
             p.PrintPatternKWD();
+                    break;
 
-
+                case 2:
             LeapYearOrNot l = new LeapYearOrNot();
             l.CheckLeapYear();
+                    break;
 
-            SpringSeason s = new SpringSeason();
+                case 3:
+                    SpringSeason s = new SpringSeason();
             s.CheckDayAndMonth();
+                    break;
 
-            Quadratic q = new Quadratic();
-            q.FindRoots();
+                case 4:
+             Quadratic q = new Quadratic();
+             q.FindRoots();
+                    break;
 
+
+                case 5:
+            PrintThreeNames print = new PrintThreeNames();
+            print.printNames();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter valid input ");
+                    break;
+        }
         }   
     }
 }
